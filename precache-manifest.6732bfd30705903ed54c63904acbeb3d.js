b3ccf2250f82488f9c39e6ca0ee5aa165685edb0
gmarszalek <grzegorzmarszalek92@gmail.com>
2020-09-22 22:42:28 +0200

    Updates

---
+++ precache-manifest.6732bfd30705903ed54c63904acbeb3d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d8abe9487295b2e8646e912b06a3b42",
+    "url": "/react-flashcard-app/index.html"
+  },
+  {
+    "revision": "130594175f6f8950cc06",
+    "url": "/react-flashcard-app/static/css/main.5f27fb9c.chunk.css"
+  },
+  {
+    "revision": "d6e7bcda1a2f44299d31",
+    "url": "/react-flashcard-app/static/js/2.69aae7e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-flashcard-app/static/js/2.69aae7e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "130594175f6f8950cc06",
+    "url": "/react-flashcard-app/static/js/main.dd86e4f9.chunk.js"
+  },
+  {
+    "revision": "3b1db16f5baea4d17f7b",
+    "url": "/react-flashcard-app/static/js/runtime-main.c28d7b2e.js"
+  }
+]);
